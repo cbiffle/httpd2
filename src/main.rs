8146616dca3dcf98a67515b1f912ec4a2884b679
Cliff L. Biffle <code@cliffle.com>
2020-01-08 15:32:30 -0800

    chroot

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::fs;
 use std::io;
 use std::sync::Arc;
 
-use futures::TryStreamExt;
-
 use hyper::{Body, Method, Request, Response, StatusCode};
 use hyper::error::Error;
 use hyper::service::{service_fn};
@@ -21,27 +19,6 @@ async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Error> {
         (&Method::GET, "/") => {
             *response.body_mut() = Body::from("try POSTing data to /echo");
         }
-        (&Method::POST, "/echo") => {
-            *response.body_mut() = req.into_body();
-        }
-        (&Method::POST, "/echo/uppercase") => {
-            let mapping = req
-                .into_body()
-                .map_ok(|chunk| {
-                    chunk.iter()
-                        .map(|byte| byte.to_ascii_uppercase())
-                        .collect::<Vec<_>>()
-                });
-            *response.body_mut() = Body::wrap_stream(mapping);
-        }
-        (&Method::POST, "/echo/reverse") => {
-            let full_body = hyper::body::to_bytes(req.into_body()).await?;
-            let reversed = full_body.iter()
-                .rev()
-                .cloned()
-                .collect::<Vec<_>>();
-            *response.body_mut() = reversed.into();
-        }
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
         }
@@ -52,6 +29,8 @@ async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Error> {
 
 #[tokio::main]
 async fn main() {
+    let path = std::env::args().nth(1);
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     let mut config = ServerConfig::new(NoClientAuth::new());
@@ -68,6 +47,10 @@ async fn main() {
     config.set_single_cert(cert_chain, key).expect("can't set cert");
     config.versions = vec![ProtocolVersion::TLSv1_3, ProtocolVersion::TLSv1_2];
     config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+
+    if let Some(path) = path {
+        nix::unistd::chroot(&*path).expect("can't chroot");
+    }
 
     let tls_acceptor = TlsAcceptor::from(Arc::new(config));
 
