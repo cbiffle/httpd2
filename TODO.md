949c498cc9fabe504f53d450331efb198773edc5
Cliff L. Biffle <code@cliffle.com>
2020-01-09 11:05:08 -0800

    Percent decoding.

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
-- Implement percent decoding and sanitization.
-
 - Parse host from requests, serve different roots per host.
 
 - Customizable xtension to mimetype mapping.
--- src/main.rs
+++ src/main.rs
@@ -283,7 +283,84 @@ impl<I: Iterator<Item = char>> Iterator for Sanitizer<I> {
 
 
 fn sanitize_path(path: &str) -> String {
-    Sanitizer::from(path.chars()).collect()
+    Sanitizer::from(PercentDecoder::from(path.chars())).collect()
+}
+
+struct PercentDecoder<I> {
+    inner: I,
+    state: PercentState,
+}
+
+impl<I> From<I> for PercentDecoder<I> {
+    fn from(inner: I) -> Self {
+        Self {
+            inner,
+            state: PercentState::Normal,
+        }
+    }
+}
+
+enum PercentState {
+    /// Haven't seen a percent escape recently.
+    Normal,
+    /// A percent escape was found to be invalid on its final character. We have
+    /// yielded the original '%' and need to yield these additional characters
+    /// in sequence before touching `inner`.
+    Unspool2(char, char),
+    /// A percent escape was found to be invalid. We have yielded some portion
+    /// of it literally, and still need to yield this char before touching
+    /// `inner`.
+    Unspool(char),
+}
+
+impl<I: Iterator<Item = char>> Iterator for PercentDecoder<I> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        fn hexit(c: char) -> Option<u8> {
+            match c {
+                '0'..='9' => Some(c as u8 - '0' as u8),
+                'A'..='F' => Some(c as u8 - 'A' as u8 + 10),
+                'a'..='f' => Some(c as u8 - 'a' as u8 + 10),
+                _ => None,
+            }
+        }
+
+        match self.state {
+            PercentState::Normal => {
+                match self.inner.next()? {
+                    '%' => {
+                        if let Some(x) = self.inner.next() {
+                            if let Some(y) = self.inner.next() {
+                                if let (Some(x), Some(y)) = (hexit(x), hexit(y)) {
+                                    return Some((x << 4 | y) as char)
+                                } else {
+                                    self.state = PercentState::Unspool2(x, y);
+                                }
+                            } else {
+                                self.state = PercentState::Unspool(x);
+                            }
+                        }
+                        Some('%')
+                    }
+                    c => Some(c)
+                }
+            }
+            PercentState::Unspool2(x, y) => {
+                self.state = PercentState::Unspool(y);
+                Some(x)
+            }
+            PercentState::Unspool(y) => {
+                self.state = PercentState::Normal;
+                Some(y)
+            }
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let (min, max) = self.inner.size_hint();
+        (min / 3, max)
+    }
 }
 
 /// Attempts to serve a file in response to `req`.
@@ -600,5 +677,25 @@ mod tests {
         assert_eq!(sanitize_path("/\0"), "./_");
 
         assert_eq!(sanitize_path("//.././doc.pdf\0/"), "./:./:/doc.pdf_/");
+    }
+
+    #[test]
+    fn percent_decode() {
+        assert_eq!(sanitize_path(""), "./");
+        assert_eq!(sanitize_path("%"), "./%");
+        assert_eq!(sanitize_path("%4"), "./%4");
+        assert_eq!(sanitize_path("%41"), "./A");
+        assert_eq!(sanitize_path("%4a"), "./J");
+        assert_eq!(sanitize_path("%4A"), "./J");
+        assert_eq!(sanitize_path("%4g"), "./%4g");
+        assert_eq!(sanitize_path("%2525"), "./%25");
+    }
+
+    #[test]
+    fn percent_and_sanitize() {
+        assert_eq!(sanitize_path("%2f"), "./");
+        assert_eq!(sanitize_path("%2f%2F"), "./");
+        assert_eq!(sanitize_path("%2f%2e%2e"), "./:.");
+        assert_eq!(sanitize_path("%2f%2e%2e%00"), "./:._");
     }
 }
