e1ca962ae1b760feb1c02efc0ad1b2e42dd98224
Cliff L. Biffle <code@cliffle.com>
2021-05-18 10:33:56 -0700

    Add WASM module mime type (whoops).

--- src/serve.rs
+++ src/serve.rs
@@ -326,6 +326,7 @@ fn map_content_type(path: &Path) -> &'static str {
         Some("woff2") => "font/woff2",
         Some("png") => "image/png",
         Some("xml") => "application/xml",
+        Some("wasm") => "application/wasm",
         _ => "text/plain",
     }
 }
