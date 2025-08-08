9be089f30e25fa8163ce4e10f9e4c1e40e5067be
Cliff L. Biffle <code@cliffle.com>
2025-08-08 08:43:09 -0700

    Tentatively fix compile on MacOS (macOS? MaCoS?)
    Thanks to @jedahan on Github for this suggestion.

--- src/security.rs
+++ src/security.rs
@@ -13,7 +13,32 @@ pub fn drop_privs(log: &slog::Logger, args: &CommonArgs) -> Result<(), ServeErro
     }
     if let Some(gid) = args.gid {
         nix::unistd::setgid(gid)?;
-        nix::unistd::setgroups(&[gid])?;
+
+        cfg_if::cfg_if! {
+            if #[cfg(target_os = "macos")] {
+                // Darwin-based platforms have done something goofy with
+                // setgroups, and nix have decided not to include it for some
+                // reason, instead citing some Mac-specific daemon you're
+                // supposed to talk to.
+                //
+                // But our use case of "set a single group as a root user"
+                // appears to be well supported, so, it seems like we can get
+                // basic Darwin support by bypassing nix.
+                //
+                // Safety: this call is unsafe because it is equivalent to
+                // dereferencing a raw pointer. libc provides no explicit safety
+                // docs (Boooooo) but from the POSIX docs, I can infer that the
+                // contract we need to uphold here is: the pointer we pass is
+                // valid, the count is correct, and the pointed-to data is
+                // proper size_ts.
+                if unsafe { libc::setgroups(1, &gid.as_raw()) } != 0 {
+                    return Err(std::io::Error::last_os_error().into());
+                }
+            } else {
+                // Normal POSIX platforms:
+                nix::unistd::setgroups(&[gid])?;
+            }
+        }
     }
     if let Some(uid) = args.uid {
         nix::unistd::setuid(uid)?;
